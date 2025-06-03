40b4f21206ded2139420b6632011251e6de2ee7d
GonzaloSoto97 <38223213+GonzaloSoto97@users.noreply.github.com>
2025-06-03 19:33:34 -0300

    consigna en main.c

--- Laboratorio3/main/main.c
+++ Laboratorio3/main/main.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
 
+// nueva Task A. Esta tarea se encarga de llamar a la task C y 
+// agregarle un delay a la tarea antes de mandarla a la queue.
+void vTaskA( void *pvParameters ) {
+    while(1) {
+            for(;;);
+        }
+    vTaskDelete(NULL);
+}
+
+
+// nueva Task C. Esta tarea se encarga de de encender y apagar un LED, 
+// y ponerlo con un color que lee de una queue.
+void vTaskC( void *pvParameters ) {
+    while(1) {
+            for(;;);
+        }
+    vTaskDelete(NULL);
+}
+
+// nueva Task B.
+// Esta tarea se encargará de gestionar el USB/UART y procesar los comandos que le llegan.
+// Si el parámetro de tiempo es cero, TASK B colocará directamente el comando en el
+// queue. Si el parámetro de tiempo es distinto de cero, TASK B creará un timer, y será el
+// callback del timer que pondrá el comando en el queue.
+void vTaskB( void *pvParameters ) {
+    while(1) {
+            for(;;);
+        }
+    vTaskDelete(NULL);
+}
+
 void app_main(void)
 {
     
