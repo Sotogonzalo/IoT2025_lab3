1891bd0523b87cc37ee810a41aee5bee687d8cfe
GonzaloSoto97 <38223213+GonzaloSoto97@users.noreply.github.com>
2025-06-07 16:30:45 -0300

    Inicialización de usb/uart en main.

--- Laboratorio3/main/main.c
+++ Laboratorio3/main/main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include "esp_log.h"
+#include "led_embebido.h"
+#include "esp_rom_delay_ms.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+
+#define UART_PORT UART_NUM_0
+#define UART_TXD  (GPIO_NUM_1)
+#define UART_RXD  (GPIO_NUM_3)
+#define UART_BAUD_RATE 115200
+#define UART_BUF_SIZE 1024
+
 
 // nueva Task A. Esta tarea se encarga de llamar a la task C y 
 // agregarle un delay a la tarea antes de mandarla a la queue.
@@ -8,7 +21,6 @@ void vTaskA( void *pvParameters ) {
         }
     vTaskDelete(NULL);
 }
-
 
 // nueva Task C. Esta tarea se encarga de de encender y apagar un LED, 
 // y ponerlo con un color que lee de una queue.
@@ -33,5 +45,19 @@ void vTaskB( void *pvParameters ) {
 
 void app_main(void)
 {
+    uart_config_t uart_config = {
+        .baud_rate = UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+
+    // Driver UART sin eventos
+    ESP_ERROR_CHECK(uart_driver_install(UART_PORT, UART_BUF_SIZE * 2, 0, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_param_config(UART_PORT, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_PORT, UART_TXD, UART_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
     
+    //uart_read_bytes() y uart_write_bytes()
 }
